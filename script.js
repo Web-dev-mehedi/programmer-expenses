5ace1508b64225432d123abf1c380d37ab5ddfb4
mehedihasan <mehedihasan645356@gmail.com>
2024-09-21 20:33:03 +0600

    correction

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
-document.getElementById('Assistant').addEventListener('click', function(){
+    document.getElementById('Assistant').addEventListener('click', function(){
    
    
     document.getElementById('hidden').classList.add('hidden');
     document.getElementById('top').classList.remove('hidden');
     document.getElementById('top1').classList.remove('hidden');
     document.getElementById('hidden1').classList.add('hidden');
-    document.getElementById('history').classList.remove('bg-red-300');
-    document.getElementById('Assistant').classList.add('bg-red-300');
+    document.getElementById('Assistant').classList.add('bg-gradient-to-r', 'from-blue-500','to-violet-700','text-white');
+    document.getElementById('history').classList.remove('bg-gradient-to-r', 'from-violet-500','to-blue-700','text-white');
+    // document.getElementById('Assistant').classList.add();
    
 
 })
 
-document.getElementById('history').addEventListener('click', function(){
+   document.getElementById('history').addEventListener('click', function(){
     
    document.getElementById('hidden').classList.remove('hidden');
    document.getElementById('top').classList.add('hidden');
    document.getElementById('top1').classList.add('hidden');
    document.getElementById('hidden1').classList.remove('hidden');
+   document.getElementById('history').classList.add('bg-gradient-to-r', 'from-violet-500','to-blue-700','text-white');
+   document.getElementById('Assistant').classList.remove('bg-gradient-to-r', 'from-blue-500','to-violet-700','text-white');
 
-   document.getElementById('Assistant').classList.remove('bg-red-300');
-   document.getElementById('history').classList.add('bg-red-300');
 
 
 
@@ -28,7 +29,7 @@ document.getElementById('history').addEventListener('click', function(){
 
 
 //for  calculate
-document.getElementById('Calculate').addEventListener('click', function(event){
+    document.getElementById('Calculate').addEventListener('click', function(event){
     event.preventDefault();
     const income =  parseFloat(document.getElementById('income').value);
     // console.log(typeof income)
@@ -38,35 +39,47 @@ document.getElementById('Calculate').addEventListener('click', function(event){
       
        
     const allExpenses = software + courses + internet;
-        if(allExpenses > income){
-         alert('YOu have NO money')
-              return
-        }
-        const avaiableBalance = income - allExpenses;
+    if(allExpenses > income){
+    alert('YOu have NO money')
+    return
+    }
+    const avaiableBalance = income - allExpenses;
 
  // history
 
  // checking 
- if(isNaN(allExpenses )){
+    if(isNaN(allExpenses ),isNaN(income)){
     alert('Pls fill all the Fields')
-     return;
-     
- }
+    return;
 
- else{
+    }
+
+    else{
     const output =document.getElementById('append')
 
     const div = document.createElement('div');
-    div.classList.add('bg-white','w-[45%]','border-l-2' ,'rounded-xl','p-6','mb-6','ml-4','inline-block')
-   //  div.setAttribute('class','inner-tt')
+    div.classList.add('bg-white','w-[45%]','border-l-2' ,'rounded-xl','p-6','mb-6','ml-4','inline-block');
+
+    const time= new Date().toLocaleString('en-us',{
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        //   second: '2-digit',
+        hour12: true // Use 24-hour format
+    });
+
+ 
     div.innerHTML = `
-         <p class="text-black text-3xl font-bold underline mb-4"> Expenses<p
-        <h2 class=" text-gray-700 text-2xl font-semibold capitalize "> income : ${income} </h2>
-        <h3 class="text-gray-700 text-2xl font-semibold capitalize ">expenses : ${allExpenses}</h3>
-        <h3 class="text-gray-700 text-xl font-semibold capitalize ">  Remaining Balance : ${avaiableBalance}</h3>
+        <p class="text-black text-3xl font-bold underline mb-4">  Expenses 💸<p
+         <p class="text-black text-base font-bold underline mb-4">Date & time: ${time} <p
+        <h2 class=" text-gray-700 text-2xl font-semibold capitalize "> income : $${income} </h2>
+        <h3 class="text-gray-700 text-2xl font-semibold capitalize ">expenses : $${allExpenses}</h3>
+        <h3 class="text-gray-700 text-xl font-semibold capitalize ">  Remaining Balance : $${avaiableBalance}</h3>
         `
     output.appendChild(div);
- }
+    }
 
 //  document.getElementsByClassName("inner-tt").style.cssText= `
 //  width:50%;
@@ -79,9 +92,9 @@ document.getElementById('Calculate').addEventListener('click', function(event){
 // `;
 //
 
-   document.getElementById('hidden').classList.remove("hidden")
-   document.getElementById('Remaining-Balance').innerText=avaiableBalance;
-   document.getElementById('expenses').innerText= allExpenses ;
+    document.getElementById('hidden').classList.remove("hidden")
+    document.getElementById('Remaining-Balance').innerText=avaiableBalance;
+    document.getElementById('expenses').innerText= allExpenses ;
     document.getElementById('balance').innerText= allExpenses;
     document.getElementById('Saving-Amount').innerText= "0";
    
@@ -92,7 +105,7 @@ document.getElementById('Calculate').addEventListener('click', function(event){
 
 
 // for Calculate-savings
-document.getElementById('Calculate-savings').addEventListener('click', function(event){
+    document.getElementById('Calculate-savings').addEventListener('click', function(event){
  
     const income =  parseFloat(document.getElementById('income').value);
     const software =  parseFloat(document.getElementById('Software').value);
@@ -111,31 +124,42 @@ document.getElementById('Calculate-savings').addEventListener('click', function(
      // history
 
  // checking 
- if(isNaN(savings)){
+    if(isNaN(savings)){
     return;
-    alert('Pls fill all the Fields')
-     
-     
- }
+    // alert('Pls fill all the Fields')  
 
- else{
+    }
+
+    else{
     const output =document.getElementById('append')
-
+    
     const div = document.createElement('div');
-    div.classList.add('bg-white','w-[45%]','border-l-2' ,'rounded-xl','p-6','mb-6','ml-4','inline-block')
-   //  div.setAttribute('class','inner-tt')
+    div.classList.add('bg-white','w-[45%]','border-l-2' ,'rounded-xl','p-6','mb-6','ml-4','inline-block');
+    const time= new Date().toLocaleString('en-us',{
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        //   second: '2-digit',
+        hour12: true // Use 24-hour format
+    });
+
+   
+
     div.innerHTML = `
-         <p class="text-black text-3xl font-bold underline mb-4"> Expenses<p
-        <h2 class=" text-gray-700 text-2xl font-semibold capitalize "> income : ${income} </h2>
-        <h3 class="text-gray-700 text-2xl font-semibold capitalize ">expenses : ${tWithSavings}</h3>
-        <h3 class="text-gray-700 text-2xl font-semibold capitalize ">savings : ${savingAmount}</h3>
-        <h3 class="text-gray-700 text-xl font-semibold capitalize ">  Remaining Balance : ${avaiableBalance}</h3>
+            <p class="text-black text-3xl font-bold underline mb-4"> Expenses 💸<p
+            <p class="text-black text-base font-bold underline mb-4">Date & time: ${time} <p
+            <h2 class=" text-gray-700 text-2xl font-semibold capitalize "> income : $${income} </h2>
+            <h3 class="text-gray-700 text-2xl font-semibold capitalize ">expenses : $${tWithSavings}</h3>
+            <h3 class="text-gray-700 text-2xl font-semibold capitalize ">savings : $${savingAmount}</h3>
+            <h3 class="text-gray-700 text-xl font-semibold capitalize ">  Remaining Balance : $${avaiableBalance}</h3>
         `
     output.appendChild(div);
- }
+    }
   
- document.getElementById('hidden').classList.remove("hidden")
-   document.getElementById('expenses').innerText= tWithSavings ;
+    document.getElementById('hidden').classList.remove("hidden")
+    document.getElementById('expenses').innerText= tWithSavings ;
     document.getElementById('balance').innerText= allExpenses;
     document.getElementById('Saving-Amount').innerText= savingAmount;
     document.getElementById('Remaining-Balance').innerText= avaiableBalance;
